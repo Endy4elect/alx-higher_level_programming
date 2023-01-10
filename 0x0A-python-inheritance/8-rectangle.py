29a7d2f313ad3338098f316e76e80335f94cbb26
Endy4elect <endy4elect@yahoo.com>
2023-01-10 06:23:10 -0800

    8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -7,9 +7,9 @@ class Rectangle(BaseGeometry):
     """a class to define rectangle using BaseGeometry"""
 
     def __init__(self, width, height):
-    """Intialize a new Rectangle
-    """
-    self.integer_validator("width", width)
-    self.__width = width
-    self.integer_validator("height", height)
-    self.__height = height
+        """Intialize a new Rectangle
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
