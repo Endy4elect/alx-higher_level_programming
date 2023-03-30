db9865fe47718238e7ec4bbd354240d18f8f8ac6
Endy4elect <endy4elect@yahoo.com>
2023-03-30 20:49:50 +0000

    python network

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
+# makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
 curl -s -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me --header "Origin: HolbertonSchool"
