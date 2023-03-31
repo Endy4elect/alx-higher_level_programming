fabff1830f736b95ff1dcc7d0fce194399eff1d2
Endy4elect <endy4elect@yahoo.com>
2023-03-31 21:27:24 +0000

    hbtn header

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
-import requests
-import sys
+"""Uses the GitHub API to display a GitHub ID based on given credentials.
+Usage: ./10-my_github.py <GitHub username> <GitHub password>
+  - Uses Basic Authentication to access the ID.
+"""
 from requests.auth import HTTPBasicAuth
+import sys
+import requests
 
-if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print('Usage: python github_id.py <username> <personal_access_token>')
-        sys.exit(1)
 
-    username = sys.argv[1]
-    access_token = sys.argv[2]
-
-    url = f'https://api.github.com/users/{username}'
-    headers = {'Authorization': f'Basic {access_token}'}
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        user_info = response.json()
-        print(f"User ID: {user_info['id']}")
-    else:
-        print(f"Error: {response.status_code}")
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,19 +2,20 @@
 """Lists the 10 most recent commits on a given GitHub repository.
 Usage: ./100-github_commits.py <repository name> <repository owner>
 """
-import requests
 import sys
+import requests
 
-url = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits"
 
-response = requests.get(url)
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-if response.status_code == 200:
-    commits = response.json()
-
-    for commit in commits[:10]:
-        sha = commit['sha']
-        author_name = commit['commit']['author']['name']
-        print(f"{sha}: {author_name}")
-else:
-    print(f"Error: {response.status_code}")
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
