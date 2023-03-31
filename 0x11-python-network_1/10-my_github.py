948292ab50019640cedaa9ae026b85ce98a36c1e
Endy4elect <endy4elect@yahoo.com>
2023-03-31 20:33:54 +0000

    hbtn header

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import requests
 import sys
+from requests.auth import HTTPBasicAuth
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Lists the 10 most recent commits on a given GitHub repository.
+Usage: ./100-github_commits.py <repository name> <repository owner>
+"""
+import requests
+import sys
+
+url = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    commits = response.json()
+
+    for commit in commits[:10]:
+        sha = commit['sha']
+        author_name = commit['commit']['author']['name']
+        print(f"{sha}: {author_name}")
+else:
+    print(f"Error: {response.status_code}")
